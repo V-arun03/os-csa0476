e68d50d4a375826f4ac9849c734a517d50cbd978
V-arun03 <112625305+V-arun03@users.noreply.github.com>
2022-10-06 12:24:08 +0530

    Add files via upload

---
+++ round robin program.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>  
+#include<conio.h>  
+  int main()  
+{   
+    int a[3];   
+    printf(" Total number of process in the system: ");  
+    scanf("%d", &NOP);  
+
+    
+for(i=0;i<;i++) 
+{  
+printf("\n Enter the Arrival and Burst time of the Process[%d]\n", i+1);  
+printf(" Arrival time is: \t");   
+scanf("%d", &at[i]);  
+printf(" \nBurst time is: \t"); 
+scanf("%d", &bt[i]);  
+temp[i] = bt[i];   
+}   
+
+   
